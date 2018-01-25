250b174ad756b02983bf1f098d0d561022257111
root <root@ip-172-31-7-213.us-west-2.compute.internal>
2018-01-25 18:30:34 +0000

    Agregando CLIDumper.

---
+++ framework/Component/VarDumper/Dumper.php
@@ -0,0 +1,29 @@
+<?php
+
+    namespace Framework\Component\VarDumper;
+
+    use Symfony\Component\VarDumper\Cloner\VarCloner;
+    use Symfony\Component\VarDumper\Dumper\CliDumper;
+    use Symfony\Component\VarDumper\Dumper\HtmlDumper;
+
+    class Dumper {
+
+
+
+        /**
+         * Dump a value with elegance.
+         *
+         * @param  mixed  $value
+         * @return void
+         */
+        public function dump($value)
+        {
+            if (class_exists(CliDumper::class)) {
+                $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper;
+
+                $dumper->dump((new VarCloner)->cloneVar($value));
+            } else {
+                var_dump($value);
+            }
+        }
+    }
